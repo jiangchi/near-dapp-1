bcd7c967d33b630e1b8c6df80a2cef17824affea
jiangchister <jiangchister@gmail.com>
2022-07-01 13:32:14 +0800

    fix bug

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/rust_template.wasm ./res/
 
 near login //第一次需要登录，之后合约修改，不需要此步骤
-near deploy --wasmFile target/wasm32-unknown-unknown/release/rust_template.wasm --accountId putong333.testnet
+near deploy --wasmFile target/wasm32-unknown-unknown/release/near-dapp-1.wasm --accountId putong333.testnet
 
 near call putong333.testnet get_puzzle_number  --accountId putong333.testnet
 //注意cmd下需要将“转义，加\
